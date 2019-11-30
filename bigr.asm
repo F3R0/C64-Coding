581b92015c309f4944c435e130536d67d7112837
pixelfero <ferhattanman@1ateknik.com.tr>
2019-11-30 14:45:29 +0300

    Fixes

--- bigr.asm
+++ bigr.asm
@@ -1,4 +1,3 @@
-bigr:
 
 initSpritesIntro:
 
@@ -10,7 +9,7 @@ initSpritesIntro:
 	sta $d027+i
 	}
 
-	lda	#%11111111
+	lda	#%00111111
 	sta $d015
 
 	lda #%00000000
@@ -31,11 +30,11 @@ initSpritesIntro:
     sta $d00a
 
 
-	lda #42
+	lda #41
 	sta $d00d
     sta $d00f
 
-    lda #84
+    lda #82
     sta $d001
     sta $d003
     adc #42
@@ -51,39 +50,91 @@ initSpritesIntro:
 	sta $d01d
 	sta $d017
 
+  RasterIRQHazirla:
+      sei
+      
+      lda #$7f
+      sta $dc0d
+      sta $dd0d
+      
+      lda $d01a
+      ora #$01
+      sta $d01a
+      
+      lda $d011
+      and #$7f
+      sta $d011
+      
+    lda #%00110101		/// no basic or kernal
+	sta $01
 
-screenScrUp:
-	jsr presents
-	ldy #0
-loop:
+      lda #$20
+      sta $d012
+
+   	lda #<irq00
+	sta $fffe
+	lda #>irq00
+	sta $ffff
+
+    cli
+
+ldx #0
+son:
+
     lda #$80
     cmp $d012
     bne *-3
-    jsr upscroll
-	iny
-	cpy #255
-	bne loop
-loop22:
-    lda #$80
-    cmp $d012
-    bne *-3
-    jsr upscroll
-	dey
-	cpy #1
-	bne loop22
-    jmp bigrend
+
+      txa
+      pha
+      jsr $1003
+      jsr upscroll
+      
+      pla
+      tax
+      inx
+      cpx #255
+      bne son 
+
+jmp son
+  
+
+
+irq00:
+
+        sta irq00a
+		stx irq00x
+		sty irq00y
+
+        lsr $d019
+ 
+jsr colcyc
+jsr updatetxt
+
+
+
+     	irq00a_p: lda #$00
+		.label irq00a = irq00a_p+1
+
+		irq00x_p: ldx #$00
+		.label irq00x = irq00x_p+1
+
+		irq00y_p: ldy #$00
+		.label irq00y = irq00y_p+1
+
+		rti
 
 
 upscroll:
-		
+
         lda ydelay    //set out a delay for the upscroll
         cmp #$2			//delayed enough? because upscroll is faster
         beq ydelayok    //compared to side scroll.
         inc ydelay    //increment delay by 1 byte until 8
         rts
-
 ydelayok:
-
+        jsr checktimer
+              // are you ready to change the text?
         lda #$00      //reset delay
         sta ydelay
         lda ypos      //set y-position for upwards scroll
@@ -94,6 +145,7 @@ ydelayok:
         sta ypos //for smooth scroll.
         rts
 doscroll:
+        
         lda #$17 //reset value of y pos for scroll
         sta ypos
 
@@ -109,7 +161,7 @@ puttxt:
                 inx
                 cpx #$b
                 bne puttxt
-				
+
                 ldx #$00
 scrread:
         lda message,x    //self modifying message ...
@@ -135,12 +187,59 @@ skipreset: //standard message found, skip reset!
         sta scrread+2
         rts
 
-presents:    
-            lda pretext,x
-            sta $440e+20*40,x
+updatetxt:    
+        ldx creditcount
+        ldy #0
+txtread:
+        lda creditstext,x    //self modifying message ...
+        sta $440e+22*40,y
+        inx
+        iny
+        cmp #$20
+        bne txtread
+        stx counter
+        
+rts
+
+checktimer:
+lda changetimer
+cmp #2
+bne updatetxt
+
+inc creditcount
+
+rts
+
+waitafewsec:
+    ldy #0
+    ldx #0
+    waitloop:	
+    dex
+    bne waitloop
+    dey
+    bne waitloop
+    rts
+
+colcyc:
+            lda colortable3
+            sta colortable3+128
+            ldx #$0
+coloop:     lda colortable3+1,x 
+            sta colortable3,x      
+            sta $d80e+20*40,x
             inx
-            cpx #11
-            bne presents
-			rts
+            cpx #128
+            bne coloop
+            rts
+
+counter
+
+creditcount:
+.byte 0
+
+changetimer:
+.byte 0
+
 
 bigrend:
+ 
